8e73058dde33b47b2bd105aee689926568a90aab
ssearobber <ssearobber@naver.com>
2020-12-06 23:13:54 +0900

    layout update

--- frontend/src/components/organisms/Header/HeaderPresenter.jsx
+++ frontend/src/components/organisms/Header/HeaderPresenter.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Profile from '../../../icons/profile.png';
 
 const HeaderPresenter = () => {
   return (
     <div className="header">
       <div className="logo">portfolio</div>
-      <div className="profile">profile</div>
+      <div className="profile">
+        <img src={Profile} alt={'profile'} />
+      </div>
     </div>
   );
 };
